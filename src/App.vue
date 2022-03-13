6efd5fd8fcf8b7662218bb99cef9abfe09b9f7c7
jahappy <j_happy-a@naver.com>
2022-03-14 00:22:47 +0900

    add sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <div id="app">
-  <router-link to="/"> 1페이지로 </router-link>
-  <router-link to="/about"> 2페이지로 </router-link>
-  <router-view/>
+  <div id="wrapper">
+    <Sidebar/>
+    <Content/>
+<!--    <router-link to="/"> 1페이지로 </router-link>-->
+<!--  <router-link to="/about"> 2페이지로 </router-link>-->
+<!--  <router-view/>-->
+
 </div>
 </template>
 
 <script>
+import Sidebar from './views/layout/sidebar';
+import Content from './views/layout/content';
 
 export default {
   name: 'App',
+  components: {
+    Sidebar,
+    Content,
+  },
   created() {
     window.$_app = this;
   },
@@ -21,4 +30,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+
 </style>
---
+++ src/views/layout/content.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- Content Wrapper -->
+  <div id="content-wrapper" class="d-flex flex-column">
+
+    <!-- Main Content -->
+    <div id="content">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "contentComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
